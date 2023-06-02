89dd61021bb631e47fc9a762cdc3878c7620673b
Adithya714 <115608981+Adithya714@users.noreply.github.com>
2023-06-02 10:58:54 +0530

    Add files via upload

---
+++ eclipse-workspace/webDriverCommands/src/main/java/Day_5/webDriverCommands/App.java
@@ -0,0 +1,13 @@
+package Day_5.webDriverCommands;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
